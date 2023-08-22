c50ae8eba38243d5d7f7b823267d3404fdcc6012
sugatbhagat20 <sugat.bhagat20@gmail.com>
2023-08-22 13:52:59 +0530

    Request / Response

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Sugat Bhagat");
+  const url = req.url;
+  if (url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>My first Page</title></head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>My first Page</title></head>");
+    res.write("<body><h1>Welcome About Us page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/node") {
+    res.write("<html>");
+    res.write("<head><title>My first Page</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
 });
 
 server.listen(4000);
