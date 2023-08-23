5717da4dabfa6181ed15befc34be29984dcddaba
sugatbhagat20 <sugat.bhagat20@gmail.com>
2023-08-24 02:29:30 +0530

    using bodyparser

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
-app.use((req, res, next) => {
-  console.log("Inside the middleware");
-  next();
-});
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
   console.log("Inside another middleware");
-  res.send("{ key1: value }");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><input type='number' name='size'><button type='submit'>Add product</button></form>"
+  );
+});
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+app.use("/", (req, res, next) => {
+  //   console.log("Inside another middleware");
+  res.send("<h1>hello from Express!</h1>");
 });
 
 app.listen(4000);
