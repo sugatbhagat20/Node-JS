a4ee781fe7fe3c0628095379c240ff179785b0d1
sugatbhagat20 <sugat.bhagat20@gmail.com>
2023-08-25 10:53:44 +0530

    Routers and Filters

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/add-product", (req, res, next) => {
-  console.log("Inside another middleware");
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><input type='number' name='size'><button type='submit'>Add product</button></form>"
-  );
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-app.use("/", (req, res, next) => {
-  //   console.log("Inside another middleware");
-  res.send("<h1>hello from Express!</h1>");
+
+app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404</h1><h1>Page Not Found</h1>");
 });
 
 app.listen(4000);
